d9b92ee5edd9bca1f03624ac9d567d7fa4b785dc
Chetan Sarva <chetan@pixelcop.net>
2009-11-18 03:03:21 -0500

    scrapi fix on osx

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
 require 'stringio'
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/yahoo_sports'
+
+require "rubygems"
+require "tidy"
+
+if RUBY_PLATFORM =~ /darwin/ then
+    # fix for scrapi on Mac OS X
+    Tidy.path = "/usr/lib/libtidy.dylib" 
+end
